b1be07defc7dc271e1ab8694cf1447924de876b2
ringit-euc <108431092+ringit-euc@users.noreply.github.com>
2024-07-17 09:17:37 -0700

    Adding apps & Scripts

---
+++ scripts/post-install/postinstall.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+# Enable software updates
+softwareupdate --schedule on
+
+# Rename the mac
+sudo scutil --set ComputerName "${macName}"
+sudo scutil --set LocalHostName "${macName}"
+sudo scutil --set HostName "${macName}"
+dscacheutil -flushcache
+
+sleep 5
+
+sudo /usr/local/bin/dockutil --add '/Applications/RingCentral.app' --no-restart --allhomes
+sudo /usr/local/bin/dockutil --add '/Applications/Google Chrome.app' --no-restart --allhomes
+sudo /usr/local/bin/dockutil --remove 'Mail' --no-restart --allhomes
+
+sudo killall -KILL Dock
+
+sleep 5
+
+# Quit
+exit 0
---
+++ scripts/pre-install/preinstall.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+# Disable software updates
+softwareupdate --schedule off
+
+sleep 5
+
+# Quit
+exit 0
