e1c7bf342e506742e84193a4db92a6859eafe019
ringit-euc <108431092+ringit-euc@users.noreply.github.com>
2025-01-15 11:51:36 -0700

    Adding Set New Computer Name

--- scripts/pre-install/preinstall.sh
+++ scripts/pre-install/preinstall.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # Disable software updates
-softwareupdate --schedule off
+# softwareupdate --schedule off
 
 sleep 5
 
@@ -11,6 +11,19 @@ if [ "$arch" == "arm64" ]; then
 sudo /usr/sbin/softwareupdate --install-rosetta --agree-to-license
 fi
 
+
+# Rename the mac
+serial_number=$(ioreg -l | awk '/IOPlatformSerialNumber/ {print $4}' | tr -d '"')
+macName="LM${serial_number:3}"
+# echo "Serial Number: $macName"
+
+
+sudo scutil --set ComputerName "${macName}"
+sudo scutil --set LocalHostName "${macName}"
+sudo scutil --set HostName "${macName}"
+dscacheutil -flushcache
+
+sleep 5
 
 
 
