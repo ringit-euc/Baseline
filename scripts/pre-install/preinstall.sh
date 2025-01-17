b48094b8be3f06e4755ec431d403fce4c373eb4e
ringit-euc <108431092+ringit-euc@users.noreply.github.com>
2025-01-17 16:40:11 -0700

    Update preinstall.sh
    Updating the order, adding OS update off, and always wake up.

--- scripts/pre-install/preinstall.sh
+++ scripts/pre-install/preinstall.sh
@@ -1,16 +1,4 @@
 #!/bin/bash
-# Disable software updates
-# softwareupdate --schedule off
-
-sleep 5
-
-
-arch=$(/usr/bin/arch)
- 
-if [ "$arch" == "arm64" ]; then
-sudo /usr/sbin/softwareupdate --install-rosetta --agree-to-license
-fi
-
 
 # Extract the last 7 characters of the serial number
 DeviceSerialNumber=$(system_profiler SPHardwareDataType | awk '/Serial/ {print substr($4, length($4)-6)}')
@@ -32,9 +20,19 @@ scutil --set HostName "$computerName"
 # Flush the DNS cache
 dscacheutil -flushcache
 
-sleep 5
+# Install Rosetta 2 for M1 device
+arch=$(/usr/bin/arch)
+ 
+if [ "$arch" == "arm64" ]; then
+sudo /usr/sbin/softwareupdate --install-rosetta --agree-to-license
+fi
 
+# Disable software updates
+softwareupdate --schedule off
 
+# Let's not go to sleep
+caffeinate -d -i -m -s -u &
+caffeinatepid=$!
 
 # Quit
 exit 0
